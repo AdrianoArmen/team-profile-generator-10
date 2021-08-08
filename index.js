2b9ba01606a463bfb08e59f5426dd1ddd8b186f6
AdrianoA <adriano.armendaris@gmail.com>
2021-08-08 05:08:05 -0500

    packages and classes imported on index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// packages required for app development
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// import classes from lib directory
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// team array declared to get the team objects
+const teamArray = [];
