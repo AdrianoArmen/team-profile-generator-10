ba173c04d8b97a30b2a6861f485cd55e92853e36
AdrianoA <adriano.armendaris@gmail.com>
2021-08-09 00:38:51 -0500

    build manager questions to prompt

--- index.js
+++ index.js
@@ -10,3 +10,28 @@ const Intern = require("./lib/Intern");
 // team array declared to get the team objects
 const teamArray = [];
 
+// build Manager by quistionnaire
+function buildManager() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is the team manager's name?",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "What is the team manager's id?",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is the team manager's email?",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "What is the team manager's office number?",
+    },
+  ]);
+}
