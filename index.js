e9e58a2205f6dec6e4be9c4681ada589fe676ebf
AdrianoA <adriano.armendaris@gmail.com>
2021-08-09 00:51:49 -0500

    build engineer by prompted questions

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Intern = require("./lib/Intern");
 // team array declared to get the team objects
 const teamArray = [];
 
-// build Manager by quistionnaire
+// build Manager by prompted questionnaire
 function buildManager() {
   inquirer.prompt([
     {
@@ -32,6 +32,32 @@ function buildManager() {
       type: "input",
       name: "managerOfficeNumber",
       message: "What is the team manager's office number?",
+    },
+  ]);
+}
+
+// build Engineer by prompted questionnaire
+function buildEngineer() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "What is your engineer's name?",
+    },
+    {
+      type: "input",
+      name: "engineerId",
+      message: "What is your engineer's id?",
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "What is your engineer's email?",
+    },
+    {
+      type: "input",
+      name: "engineerGithub",
+      message: "What is your engineer's GitHub username?",
     },
   ]);
 }
