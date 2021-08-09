99f2856027cf6ed8a0153d8aeb023bbeff31888c
AdrianoA <adriano.armendaris@gmail.com>
2021-08-09 05:19:34 -0500

    html template file added and basic html structure built

---
+++ src/html-template.js
@@ -0,0 +1,38 @@
+// new team to be generated on html file
+const teamGenerator = (team) => {};
+
+module.exports = (team) => {
+    // html basic structure - bootstrap and fontawesome imported
+  return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-heading">
+                <h1 class="text-center">My Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-area col-12 d-flex justify-content-center">
+                ${teamGenerator(team)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+};
