290d3b76a911657107203eabd58bf2094d217d80
AdrianoA <adriano.armendaris@gmail.com>
2021-08-09 00:57:08 -0500

    build intern by promped questionaire

--- index.js
+++ index.js
@@ -61,3 +61,29 @@ function buildEngineer() {
     },
   ]);
 }
+
+// build Inter by promped questionnaire
+function buildIntern() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "internName",
+      message: "What is your intern's name?",
+    },
+    {
+      type: "input",
+      name: "internId",
+      message: "What is your intern's id?",
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: "What is your intern's email?",
+    },
+    {
+      type: "input",
+      name: "internSchool",
+      message: "What is your intern's school?",
+    },
+  ]);
+}
