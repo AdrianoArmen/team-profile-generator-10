c2eeb8f4ebfcd40604e0852cf89bb835f9cde34b
AdrianoA <adriano.armendaris@gmail.com>
2021-08-10 01:14:20 -0500

    manager generator fucntion to create html card added

--- src/html-template.js
+++ src/html-template.js
@@ -1,5 +1,35 @@
 // new team to be generated on html file
-const teamGenerator = (team) => {};
+const teamGenerator = (team) => {
+
+    // card html structure for manager
+    const managerGenerator = manager => {
+        return `
+        <div class="card">
+        <div class="card-header">
+            <h2 class="card-title">${manager.getName()}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+        </div>
+        <div class="card-body">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${manager.getId()}</li>
+                <li class="list-group-item">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+                <li class="list-group-item">Office number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+    </div>
+        `;
+    };
+
+ 
+    const html = [];
+
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => managerGenerator(manager))
+    );
+   
+
+};
 
 module.exports = (team) => {
     // html basic structure - bootstrap and fontawesome imported
